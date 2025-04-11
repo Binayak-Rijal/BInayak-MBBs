5253dab40bda6f1b19d7ffe1e47414411f8487bf
Binayak Rijal <rijalbinayak840@gmail.com>
2025-04-11 11:51:04 +0545

    Update name.java

--- name.java
+++ name.java
@@ -1,5 +1,13 @@
 public class name {
 public static void main(String[] args) {
-    System.out.print("My best friend name is best friend ");
+    int x = 10;
+    int y = 20;
+    int z = 12;
+    if( x > y && x > z)
+        System.out.println(x);
+    else if( y > z && y > x)
+        System.out.println(y);
+    else
+        System.out.println(z);
 }
 }
